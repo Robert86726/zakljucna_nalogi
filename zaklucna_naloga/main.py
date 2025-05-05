db6058eec13d04041dffdf200e4a83fd2082520a
Robert86726 <robert.ambroz25@gmail.com>
2025-05-05 15:21:15 +0200

    preurejeno

--- zaklucna_naloga/main.py
+++ zaklucna_naloga/main.py
@@ -1,22 +1,18 @@
 from flask import Flask, render_template, request, jsonify, session, redirect, url_for
 from tinydb import TinyDB, Query
-import requests
-import os
 
 app = Flask(__name__)
-app.secret_key = "skrivni_kljuc_123"
 
-db = TinyDB('mealmatch.json')
+db = TinyDB('zapiski.json')
 users = db.table('users')
-recipes = db.table('recipes')
+notes = db.table('notes')  
 User = Query()
 
-API_KEY = "fa1b1172374a42b1b8ba88992716df46"
 
 @app.route('/')
 def home():
     if 'username' in session:
-        return redirect(url_for('search_recipes'))
+        return render_template("domov.html")
     return render_template('domov.html')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -28,16 +24,16 @@ def login():
         user = users.get(User.username == username)
         if user and user.get("password") == password:
             session['username'] = username
-            return redirect(url_for('search_recipes'))
+            return render_template('login.html')
         else:
-            return jsonify({'success': False, 'error': 'Napačno uporabniško ime ali geslo!'})
+            return render_template("napaka.html")
     
     return render_template('login.html')
 
 @app.route('/logout')
 def logout():
     session.pop('username', None)
-    return redirect(url_for('home'))
+    return render_template('domov.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -50,7 +46,7 @@ def register():
             
             user = users.get(User.username == username)
             if user:
-                return jsonify({'success': False, 'error': 'Uporabnik že obstaja!'})
+                return render_template("login.html")
             
             users.insert({
                 'username': username,
@@ -60,56 +56,20 @@ def register():
             })
             
             session['username'] = username
-            return redirect(url_for('search_recipes'))
+            return render_template('register.html')
         
         except Exception as e:
-            return jsonify({'success': False, 'error': 'Prišlo je do napake pri registraciji'})
+            return render_template('napaka.html')
     
     return render_template('register.html')
 
-@app.route('/search', methods=['GET', 'POST'])
-def search_recipes():
+@app.route('/koledar', methods=['GET', 'POST'])
+def koledar():
     if 'username' not in session:
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
 
-    found_recipes = []
-    if request.method == 'POST':
-        ingredients = request.form['ingredients']
+    return render_template("koledar.html")
 
-        url = 'https://api.spoonacular.com/recipes/findByIngredients'
-        params = {
-            'ingredients': ingredients,
-            'number': 10,
-            'apiKey': API_KEY
-        }
-
-        response = requests.get(url, params=params)
-
-        if response.status_code == 200:
-            found_recipes = response.json()
-        else:
-            found_recipes = []
-    
-    return render_template('search.html', recipes=found_recipes)
-
-@app.route('/add_recipe', methods=['GET', 'POST'])
-def add_recipe():
-    if 'username' not in session:
-        return redirect(url_for('home'))
-    
-    if request.method == 'POST':
-        name = request.form['name']
-        description = request.form['description']
-        ingredients = request.form['ingredients'].split(', ')  
-
-        recipes.insert({
-            'name': name,
-            'description': description,
-            'ingredients': [ingredient.lower() for ingredient in ingredients]  
-        })
-        return redirect(url_for('search_recipes'))
-
-    return render_template('add_recipe.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
